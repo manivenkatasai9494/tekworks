8c0cd8023843b9774d5f65a366b7856f07bbf0a0
mani <medammmani4@gmail.com>
2024-12-13 15:28:29 +0530

    to Learn Things

---
+++ toLeran.py
@@ -0,0 +1,20 @@
+
+
+Strings,
+int,
+List
+Control Statements
+IF -ELSE
+nested if
+elif
+while , for
+
+functions
+OOPS => Low Priority
+Exception Handling => Low
+Files  => Low
+Database => RDBMS (H2 , Mysql , Oracle , mySQLServer)
+Python with Data base connectivity
+Streamlit for Report Purpose
+
+ >>>project on Above Things
